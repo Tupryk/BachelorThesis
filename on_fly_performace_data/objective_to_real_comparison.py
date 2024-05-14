14561bd7d122f004a2e17b7ca27edc559379cc4a
Eckart Cobo Briesewitz <eck4art@gmail.com>
2024-05-14 15:30:01 +0200

    model comparison update

--- on_fly_performace_data/objective_to_real_comparison.py
+++ on_fly_performace_data/objective_to_real_comparison.py
@@ -2,6 +2,7 @@ import cfusdlog
 import numpy as np
 import uav_trajectory
 import matplotlib.pyplot as plt
+from residual_calculation import residual
 
 
 THREE_D = False
@@ -26,21 +27,26 @@ for t, i in zip(ts, range(0, len(ts))):
 plt.plot(evals[:, 0], evals[:, 1], label="Desired path")
 
 ### Recorded data ###
-data_paths = ["./fast/lee/log02", "./fast/scaled/log00", "./fast/nn_log00"]
-labels = ["Standard Lee controller", "Lee ctrl. + NN old", "Lee ctrl. + NN new"]
+data_paths = ["./fast/lee/log02", "./fast/nn_log00"]
+labels = ["Standard Lee controller", "Lee ctrl. + NN"]
 
 for i, data_path in enumerate(data_paths):
     data = cfusdlog.decode(data_path)['fixedFrequency']
+    print(data.keys())
 
     x = [i for i in data["stateEstimate.x"]]
     y = [i for i in data["stateEstimate.y"]]
     z = [i-1. for i in data["stateEstimate.z"]]
-    print(len(x))
-    # label = data_path.replace("./data/", "").replace("_", " ")
+    
     if THREE_D:
         ax.plot3D(x, y, z, label=labels[i])
     else:
         plt.plot(x, y, label=labels[i])
+
+    f, tau = residual(data)
+    for i, v in enumerate(["x", "y", "z"]):
+        f_i = [f_[i] for f_ in f]
+        print(f"{labels[i]} mean residual f_{v}: {sum(f_i)/len(f_i)}")
 
 plt.legend()
 plt.axis('equal')
