7b35767d07c05503c034eccec1a132acb909bee0
Eckart Cobo Briesewitz <eck4art@gmail.com>
2024-05-21 12:56:28 +0200

    velocity to force comparison

--- on_fly_performace_data/vxfx.py
+++ on_fly_performace_data/vxfx.py
@@ -1,18 +1,26 @@
 import cfusdlog
+import numpy as np
 import matplotlib.pyplot as plt
 from residual_calculation import residual
 
 
-data_path = "./residual_comparison/nn_log00"
+data_path = "./nn_log04"
 data = cfusdlog.decode(data_path)['fixedFrequency']
 timestamp = data["timestamp"][1:]
 f, tau = residual(data)
 
-fig, axs = plt.subplots(3, 1, figsize=(8, 6))
 for i, v in enumerate(["x", "y", "z"]):
-    axs[i].plot(timestamp, data[f"stateEstimate.v{v}"][1:], label=f"Velocity in {v}")
-    axs[i].plot(timestamp, f[:, i], label=f"Force residual in {v}")
-    axs[i].legend()
+    plt.scatter(np.abs(data[f"stateEstimate.v{v}"][1:]), np.abs(f[:, i]), label=f"Velocity to force in {v}")
+    plt.xlabel("Velocity")
+    plt.ylabel("Force")
+    plt.show()
 
-plt.tight_layout()
+vx = data[f"stateEstimate.vx"]
+vy = data[f"stateEstimate.vy"]
+vz = data[f"stateEstimate.vz"]
+vels = [np.sqrt(vx[i]**2 + vy[i]**2 + vz[i]**2) for i, _ in enumerate(vx)]
+forces = [np.linalg.norm(f_) for f_ in f]
+plt.scatter(vels[1:], forces, alpha=.1)
+plt.xlabel("Velocity")
+plt.ylabel("Force")
 plt.show()
