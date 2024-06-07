4f59290ec61607a2219954e83cc411e42d945157
Eckart Cobo Briesewitz <eck4art@gmail.com>
2024-06-07 09:52:43 +0200

    acceleration comparison plots

---
+++ on_fly_performace_data/wbacc.py
@@ -0,0 +1,29 @@
+import rowan
+import cfusdlog
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = cfusdlog.decode("../flight_data/jana00")['fixedFrequency']
+
+acc_x = []
+acc_xb = []
+
+for i in range(1, len(data['timestamp'])):
+
+    quat = np.array([data['stateEstimate.qw'][i], data['stateEstimate.qx'][i],
+                        data['stateEstimate.qy'][i], data['stateEstimate.qz'][i]])
+    R = rowan.to_matrix(quat)
+    
+    acc = R @ np.array([data['acc.x'][i], data['acc.y'][i], data['acc.z'][i]])
+    acc[2] -= 1.
+    acc *= 9.81
+    acc_x.append(acc[0])
+    
+
+    acc = np.array([data['acc.x'][i], data['acc.y'][i], data['acc.z'][i]-1])*9.81
+    acc_xb.append(acc[0])
+
+plt.plot(acc_x, label="world")
+plt.plot(acc_xb, label="quad")
+plt.legend()
+plt.show()
